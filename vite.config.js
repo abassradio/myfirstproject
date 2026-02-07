dafeedcf696fc707a2dbb319ecc12eb7d34ace34
Abbas Mjbel <abassmjbel@gmail.com>
2026-02-07 23:51:21 +0300

    fix: remove base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/myfirstproject/', // أضف هذا السطر بالضبط (اسم المستودع بين سلاشين)
+  base: '/', 
 })
